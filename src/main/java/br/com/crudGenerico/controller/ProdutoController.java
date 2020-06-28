944897173fc7072d104c8d048fd38b8242f9bc8d
gianlucampos <gianlucampos97@gmail.com>
2020-06-28 18:37:52 -0300

    Adicionada edição e remoção de produtos

--- src/main/java/br/com/crudGenerico/controller/ProdutoController.java
+++ src/main/java/br/com/crudGenerico/controller/ProdutoController.java
@@ -4,7 +4,9 @@ import br.com.crudGenerico.models.Produto;
 import br.com.crudGenerico.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -45,4 +47,15 @@ public class ProdutoController {
         return findAll(produto);
     }
 
+    @GetMapping("/produtos/edit/{id}")
+    public ModelAndView editProduto(@PathVariable("id") Long id) {
+        Produto produto = (Produto) service.getOne(id);
+        return addProduto(produto);
+    }
+
+    @GetMapping("/produtos/delete/{id}")
+    public ModelAndView deleteProduto(@PathVariable("id") Long id) {
+        service.delete(id);
+        return findAll(null);
+    }
 }
