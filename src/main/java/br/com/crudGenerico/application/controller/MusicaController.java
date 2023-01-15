5084c3d0567f1f15ae3e4113a61df8df4b6c1988
gianlucampos <gianlucampos97@gmail.com>
2023-01-15 01:57:34 -0300

    Delete musicas funcionando

--- src/main/java/br/com/crudGenerico/application/controller/MusicaController.java
+++ src/main/java/br/com/crudGenerico/application/controller/MusicaController.java
@@ -7,9 +7,11 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @AllArgsConstructor
 @Controller
@@ -25,7 +27,7 @@ public class MusicaController {
     }
 
     @PostMapping("**/filterMusica")
-    public ModelAndView getMusicasById(@RequestParam("idMusica") Long idMusica, @RequestParam("nomeMusica") String nomeMusica) {
+    public ModelAndView getMusicasByFilter(@RequestParam("idMusica") Long idMusica, @RequestParam("nomeMusica") String nomeMusica) {
         if (idMusica == null && nomeMusica.isEmpty()) {
             return getMusicas();
         }
@@ -38,6 +40,12 @@ public class MusicaController {
         ModelAndView mv = new ModelAndView("musicas");
         mv.addObject("musicas", musicas);
         return mv;
+    }
+
+    @PostMapping("musicas/delete/{id}")
+    public RedirectView deleteMusica(@PathVariable("id") Long id) {
+        musicaService.delete(id);
+        return new RedirectView("/musicas", true);
     }
 
 }
